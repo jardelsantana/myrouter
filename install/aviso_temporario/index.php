831b753ca3e674b4e51cdc6ff1adc0c55f9bc954
MyRouter <contato@myrouter.com.br>
2019-01-30 15:59:14 -0200

    up

---
+++ install/aviso_temporario/index.php
@@ -0,0 +1,15 @@
+<?php
+// Conexão ao banco
+$link = mysql_connect('localhost','root','33#erp@myrouter#33');
+$conexao = mysql_select_db('myrouter',$link);
+if($conexao){
+    $sql = "SELECT * FROM empresa";
+    $consulta = mysql_query($sql);
+
+    $registro = mysql_fetch_assoc($consulta);
+
+    echo '<td>'.$registro["avisotemporario"].'</td>';
+
+}
+
+?>
